a35f349aa565540875a4083310abf2c3776939fd
Brad <rayray21211@gmail.com>
2025-11-17 11:03:02 +0800

    alley 4.3

--- botHandler.py
+++ botHandler.py
@@ -499,8 +499,8 @@ def alley_4():
         time.sleep(random.uniform(0.3, 0.4))
         pydirectinput.press("w")
 
-        goTo(92, 43, 1)
-        time.sleep(random.uniform(0.9, 1.1))    
+        goTo(88, 43, 1)
+        time.sleep(random.uniform(0.5, 0.6))    
         goTo(116, 40, 1)
         pydirectinput.keyDown('right')
         time.sleep(random.uniform(0.1, 0.2))
